16d23561bf883a11fc163038905d7deece200d49
Vasileios Amoiridis <vassilisamir@gmail.com>
2020-01-10 02:46:06 +0200

    added checkArgs(), parseArgs(), executeAll() and finally exeuteRecursively(). Made the code work for ';' and

--- src/myshell.c
+++ src/myshell.c
@@ -35,20 +35,26 @@
  * Functions' definitions
  *******************************************************************************
  */
-void mainLoop(int argc, char *argv[]);
-void printPromptName();
-void quitShell();
-FILE* chooseInput(int argc, char *argv[]);
-char* readLine(FILE* input);
-char** parseLine(char *line);
-int executeCmd(char **args);
+void   mainLoop        (int argc, const char *argv[]);
+void   printPromptName (void);
+void   quitShell       (void);
+FILE*  chooseInput     (int argc, const char *argv[]);
+char*  readLine        (FILE* input);
+char** parseLine       (char *line);
+int    checkArgs       (char **args);
+int    parseArgs       (char **args, char **cmd_args);
+int    executeCmd      (char **args);
+void   executeAll      (char **args);
+void executeRecursively(char **args, char **cmd_args);
+
+ int printed = 1;
 
 /*
  *******************************************************************************
  * Main Code
  *******************************************************************************
  */
-int main(int argc, char *argv[])
+int main(int argc, const char *argv[])
 {
 	mainLoop(argc, argv);
 
@@ -61,14 +67,14 @@ int main(int argc, char *argv[])
  * or until an invalid argument.
  *******************************************************************************
  */
-void mainLoop(int argc, char *argv[])
+void mainLoop(int argc, const char *argv[])
 {
 	printf("Welcome to my Shell! My name is Vasileios Amoiridis and I am the creator.\n");
 	FILE* input = chooseInput(argc, argv);
 	char* line = NULL;
 	char** args = NULL;
-	int exit_status = 0;
-	int i = 0, execute_ret = 2;
+	int exit_status = 0, check_status = 0;
+	int i = 0;
 
 	do
 	{
@@ -82,12 +88,13 @@ void mainLoop(int argc, char *argv[])
 		//next line.
 
 		args = parseLine(line);
-		while(args[i] != NULL)
-		{
-			printf("The %d argument is: %s\n",i+1,args[i]);
-			i++;
-		}
-		exit_status = executeCmd(args);
+		if (checkArgs(args)) continue; //if there is a false argument jump to
+		//next line.
+
+		executeAll(args);
+
+		free(line);
+		free(args);
 	} while(1);
 	
 }
@@ -122,7 +129,7 @@ void quitShell()
  *******************************************************************************
  */
 
-FILE* chooseInput(int argc, char* argv[])
+FILE* chooseInput(int argc, const char* argv[])
 {	
 	FILE* input;
 
@@ -131,7 +138,7 @@ FILE* chooseInput(int argc, char* argv[])
 		case 1: 			/* User typed: ./bin/myshell */
 			input = stdin;
 			break;
-		case 2: 			/* User typed: ./bin/myshell 'batchfile_name' */
+		case 2: 			/* User typed: ./bin/myshell "batchfile_name" */
 			input = fopen(argv[1], "r");
 			if(input == NULL)
 			{
@@ -140,7 +147,8 @@ FILE* chooseInput(int argc, char* argv[])
 			}
 			break;
 		default:
-			printf("Invalid input arguments.\n");
+			fprintf(stderr,RED "Invalid input arguments.\n" RESET_COLOR);
+			exit(EXIT_FAILURE);
 	}
 	return input;
 }
@@ -155,7 +163,7 @@ FILE* chooseInput(int argc, char* argv[])
  */
 char* readLine(FILE* input)
 {
-	char *line = malloc(BUFFER_SIZE * sizeof(char));
+	char *line = (char*)malloc(BUFFER_SIZE * sizeof(char));
 
 	if(line == NULL)
 	{
@@ -194,7 +202,7 @@ char* readLine(FILE* input)
  */
 char** parseLine(char *line)
 {
-	char **tokens = malloc(BUFFER_SIZE * sizeof(char*));
+	char **tokens = (char**)malloc(BUFFER_SIZE * sizeof(char*));
 	char *token = NULL;
 	int token_num = 0;
 
@@ -218,6 +226,146 @@ char** parseLine(char *line)
 
 /*
  *******************************************************************************
+ * checkArgs() is a function which checks for possible syntax errors in the
+ * arguments provided by the user.
+ *******************************************************************************
+ */
+int checkArgs(char **args)
+{
+	int check_status = 0, i = 0;
+	if(!strcmp(args[0], "&") || !strcmp(args[0], ";") ||
+	   !strcmp(args[0], "<") || !strcmp(args[0], ">") || 
+	   !strcmp(args[0], "|") || !strcmp(args[0], "&&"))
+	{
+		printf("ERROR: Bad syntax. Unexpected first character.\n");
+		check_status = 1;
+	}
+	while(args[i] != NULL)
+	{	
+		if(strstr(args[i], ";;") != NULL)
+		{	
+			printf("ERROR: Bad syntax. Two or more ';' found sequentially\n");
+			check_status = 1;
+			break;
+		}
+		else if(strstr(args[i], "&&&") != NULL)
+		{
+			printf("ERROR: Bad syntax. Three or more '&' found sequentially\n");
+			check_status = 1;
+			break;
+		}
+		else if (i > 0)
+		{
+			if(!strcmp(args[i], args[i-1]) && (!strcmp(args[i], ";") || 
+			   !strcmp(args[i], "&&")))
+			{
+				printf("ERROR: Bad syntax. Unexpected token after ; or &&.\n");
+				check_status = 1;
+				break;				
+			}
+		}
+		i++;
+	}
+	if(!strcmp(args[i-1], ";") || !strcmp(args[i-1], "&") ||
+	   !strcmp(args[i-1], "<") || !strcmp(args[i-1], ">") ||
+	   !strcmp(args[i-1], "|") || !strcmp(args[i-1], "&&"))
+	{
+		printf("ERROR: Bad syntax. Unexpected last token.\n");
+		check_status = 1;
+	}
+
+	return check_status;
+}
+
+/*
+ *******************************************************************************
+ * parseArgs()
+ *******************************************************************************
+ */
+int parseArgs(char **args, char **cmd_args)
+{
+	char **temp = (char**)malloc(BUFFER_SIZE * sizeof(char*));
+	int i = 0, j = 0, k = 0, m = 0, cnt = 0, execute_status = 0;
+
+	while(cmd_args[cnt] != NULL)
+	{
+		cmd_args[cnt] = NULL;
+		cnt++;
+	}
+
+	while(args[i] != NULL)
+	{
+		if(!(strcmp(args[i], ";")))
+		{
+			execute_status = 1;
+			break;
+		}
+		else if(!(strcmp(args[i], "&&")))
+		{
+			execute_status = 2;
+			break;
+		}
+		else if(!(strcmp(args[i], "<")))
+		{
+			execute_status = 3;
+			break;
+		}
+		else if(!(strcmp(args[i], ">")))
+		{
+			execute_status = 4;
+			break;
+		}
+		else if(!(strcmp(args[i], "|")))
+		{
+			execute_status = 5;
+			break;
+		}
+		else
+		{
+			cmd_args[i] = args[i]; //Store arguments until to find a delimiter
+			//When we find a delimiter, it means that we just parsed a command
+			//and so we have saved it in **cmd_args.
+		}
+		i++;
+	}
+	i++;   //bypass the delimiter in order to find the start of the next 
+		   //command.
+
+	while(args[i] != NULL) // args[i] is the value next to the delimiter.
+    //for example if we typed the command 'pwd ; ls -l' then the value is
+	//args[i] = "ls". We store the remainder of the line without the first
+	//command which is in **cmd_args.
+	{
+		temp[j] = args[i];
+		i++;	
+		j++;
+	}
+	while(temp[j] != NULL) //Deleting remainder values of **temp from previous
+	//function executions
+	{	
+		temp[j] = NULL;
+		j++;
+	}
+	while(temp[k] != NULL) //Now we load the cleansed **temp values in the
+	//**args variable once again.
+	{
+		args[k] = temp[k];
+		k++;
+	}
+	while(args[k] != NULL) //the remainder values of args[k] from previous
+	//function execution are being cleansed.
+	{	
+		args[k] = NULL;
+		k++;
+	}
+
+	free(temp);
+	return execute_status;
+}
+
+
+/*
+ *******************************************************************************
  * executeCmd() is a function which takes as an input the array of commands 
  * which generated from parseLine() function and executes the first command.
  * If the command was executed succesfully then the return value is 0. If 
@@ -229,7 +377,7 @@ int executeCmd(char **args)
 	pid_t pid, wait_pid;
 	int status;
 
-	if(!strcmp(args[0], "quit"))
+	if(!strcmp(*args, "quit"))
 	{
 		quitShell();
 	}
@@ -266,4 +414,71 @@ int executeCmd(char **args)
 	}
 
 	return WEXITSTATUS(status); 
+}
+
+/*
+ *******************************************************************************
+ * executeAll() is the main execute functions. It handles all the executions
+ *******************************************************************************
+ */
+void executeAll(char **args)
+{	
+	int execute_status = 0, exit_status = 0;
+	char **cmd_args = (char**)malloc(BUFFER_SIZE * sizeof(char*));
+	char **rest_args = (char**)malloc(BUFFER_SIZE * sizeof(char*));
+	int i=0,j=0,k=0;
+
+	if(cmd_args == NULL || rest_args == NULL)
+	{
+		fprintf(stderr,"ERROR: malloc() failure.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	executeRecursively(args,cmd_args);
+
+	//exit_status = executeCmd(cmd_args);
+	//printf("The exit status is: %d\n",exit_status);
+}
+
+void executeRecursively(char **args, char **cmd_args)
+{
+	int execute_status = 0, exit_status = 0, i = 0, j = 0;
+
+	execute_status = parseArgs(args, cmd_args);
+
+	printed++;
+	switch (execute_status)
+	{
+		case 0:              /* Inline command */
+			exit_status = executeCmd(cmd_args);
+			printf("The exit status is: %d\n",exit_status);
+			break;
+		case 1:              /* Commands with ';' */
+			exit_status = executeCmd(cmd_args);
+			executeRecursively(args, cmd_args);
+			break;
+		case 2:
+			exit_status = executeCmd(cmd_args);
+			if(exit_status == 0)
+			{
+				executeRecursively(args, cmd_args);
+			}
+			else
+			{
+				exit(EXIT_FAILURE);
+			}
+			break;
+		case 3:
+			printf("Not yet supported. Ciao...\n");
+			break;
+		case 4:
+			printf("Not yet supported. Ciao...\n");
+			break;
+		case 5:
+			printf("Not yet supported. Ciao...\n");
+			break;
+		default:
+			printf("ERROR: Unexpected execute status.\n");
+			exit(EXIT_FAILURE);
+	}
 }
